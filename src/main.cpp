388e39d3c428accc2bcbe60ebba9046407a26792
c0rp3n <ojhitchcock@gmail.com>
2019-12-29 12:20:06 +0000

    Update to match latest changes in Terra

--- src/main.cpp
+++ src/main.cpp
@@ -7,17 +7,18 @@
 
 int32_t main(int32_t argc, char** argv)
 {
-    auto width = 1000;
-    auto height = 500;
+    auto width = 1000.0;
+    auto height = 500.0;
     auto radius = 10.0;
     auto samples = 100;
 
     std::vector<terra::vec2> points;
     {
-        auto sampler = terra::poisson_disc_sampler(width, height, radius, samples);
-        std::count << sampler.sample();
-        points = std::move(sampler.points);
+        auto sampler = terra::poisson_disc_sampler();
+        points = sampler.sample(width, height, radius, samples);
     }
+
+    std::cout << "Points Sampled: " << points.size() << std::endl;
 
     system("pause");
 
