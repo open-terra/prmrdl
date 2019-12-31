24878142e0964d8c7fc687318762648cdec62ba8
c0rp3n <ojhitchcock@gmail.com>
2019-12-31 17:33:21 +0000

    Update to latest and add graph usage

--- src/main.cpp
+++ src/main.cpp
@@ -18,7 +18,33 @@ int32_t main(int32_t argc, char** argv)
         points = sampler.sample(width, height, radius, samples);
     }
 
-    std::cout << "Points Sampled: " << points.size() << std::endl;
+    std::cout << "Points sampled: " << points.size() << std::endl;
+
+    terra::undirected_graph graph;
+    {
+        terra::delaunator d;
+        d.triangulate(points);
+        std::cout << "Triangles created: " << d.triangles.size() << std::endl;
+
+        graph = terra::undirected_graph(points.size(), d.triangles.size());
+
+        for (size_t i = 0; i < d.triangles.size(); i+=3)
+        {
+            const size_t p0 = i;
+            const size_t p1 = i + 1;
+            const size_t p2 = i + 2;
+
+            const size_t v0 = d.triangles[p0];
+            const size_t v1 = d.triangles[p1];
+            const size_t v2 = d.triangles[p2];
+
+            graph.add_edge(v0, v1);
+            graph.add_edge(v1, v2);
+            graph.add_edge(v2, v0);
+        }
+    }
+
+    std::cout << "Graph edges: " << graph.num_edges() << std::endl;
 
     system("pause");
 
